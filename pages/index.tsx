c355ca1ea781feb7df7ed82c2342196ea13a01aa
toby <toby87@tmon.co.kr>
2022-09-27 19:02:46 +0900

    feat: Hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Dobby`s portfolio</title>
       </Head>
@@ -12,6 +13,9 @@ const Home: NextPage = () => {
       <Header />
 
       {/* hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
       {/* about */}
       {/* experience */}
       {/* skills */}
