2f63b65339de6cf90c8747dabb5fa1e5afb74306
hellomac87 <hellomac87@gmail.com>
2022-10-04 23:04:01 +0900

    feat: footer

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -10,13 +10,15 @@ function WorkExperience({}: Props) {
       initial={{ opacity: 0 }}
       transition={{ duration: 2 }}
       whileInView={{ opacity: 1 }}
-      className="h-screen flex relative overflow-hidden flex-coltext-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+      className="h-screen flex relative overflow-hidden flex-coltext-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center
+      
+      "
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Experience
       </h3>
 
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import About from "../components/About";
 import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
@@ -10,7 +11,10 @@ import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div
+      className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory z-0
+      overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+    >
       <Head>
         <title>Dobby`s portfolio</title>
       </Head>
@@ -40,6 +44,18 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-fill cuosr-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              src="https://avatars.githubusercontent.com/u/32426091?v=4"
+              alt=""
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
