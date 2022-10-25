62e3cdda72694c209ad55bc437c8d086c3ab0dc7
hellomac87 <hellomac87@gmail.com>
2022-10-25 19:46:03 +0900

    why not work

--- pages/api/getExperience.ts
+++ pages/api/getExperience.ts
@@ -1,17 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { groq } from 'next-sanity';
 import { sanityClient } from '../../sanity';
-import { Social } from '../../typings';
+import { Experience } from '../../typings';
 
 const query = groq`
-    *[_type == 'social']
+    *[_type == 'experience']{
+        ...,
+        technologies[]->
+    }
 `;
 
 type Data = {
-    socials: Social[];
+    experience: Experience[];
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    const socials: Social[] = await sanityClient.fetch(query);
-    res.status(200).json({ socials });
+    const experience: Experience[] = await sanityClient.fetch(query);
+
+    res.status(200).json({ experience });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,65 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import About from "../components/About";
-import ContactMe from "../components/ContactMe";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Projects from "../components/Projects";
-import Skills from "../components/Skills";
-import WorkExperience from "../components/WorkExperience";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import About from '../components/About';
+import ContactMe from '../components/ContactMe';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Projects from '../components/Projects';
+import Skills from '../components/Skills';
+import WorkExperience from '../components/WorkExperience';
 
 const Home: NextPage = () => {
-  return (
-    <div
-      className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory z-0
-      overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
-    >
-      <Head>
-        <title>Dobby`s portfolio</title>
-      </Head>
+    return (
+        <>
+            <div
+                className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory z-0
+      overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'
+            >
+                <Head>
+                    <title>Dobby`s portfolio</title>
+                </Head>
 
-      <Header />
+                <Header />
 
-      <section id="hero" className="snap-start">
-        <Hero />
-      </section>
+                <section id='hero' className='snap-start'>
+                    <Hero />
+                </section>
 
-      <section id="about" className="snap-center">
-        <About />
-      </section>
-      {/* experience */}
-      <section id="experience" className="snap-center">
-        <WorkExperience />
-      </section>
-      {/* skills */}
-      <section id="skills" className="snap-start">
-        <Skills />
-      </section>
-      {/* projects */}
-      <section id="projects" className="snap-start">
-        <Projects />
-      </section>
-      {/* contact me */}
-      <section id="contact" className="snap-start">
-        <ContactMe />
-      </section>
+                <section id='about' className='snap-center'>
+                    <About />
+                </section>
+                {/* experience */}
+                <section id='experience' className='snap-center'>
+                    <WorkExperience />
+                </section>
+                {/* skills */}
+                <section id='skills' className='snap-start'>
+                    <Skills />
+                </section>
+                {/* projects */}
+                <section id='projects' className='snap-start'>
+                    <Projects />
+                </section>
+                {/* contact me */}
+                <section id='contact' className='snap-start'>
+                    <ContactMe />
+                </section>
 
-      <Link href="#hero">
-        <footer className="sticky bottom-5 w-fill cuosr-pointer">
-          <div className="flex items-center justify-center">
-            <img
-              src="https://avatars.githubusercontent.com/u/32426091?v=4"
-              alt=""
-              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-            />
-          </div>
-        </footer>
-      </Link>
-    </div>
-  );
+                <Link href='#hero'>
+                    <footer className='sticky bottom-5 w-fill cuosr-pointer'>
+                        <div className='flex items-center justify-center'>
+                            <img
+                                src='https://avatars.githubusercontent.com/u/32426091?v=4'
+                                alt=''
+                                className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+                            />
+                        </div>
+                    </footer>
+                </Link>
+            </div>
+        </>
+    );
 };
 
 export default Home;
